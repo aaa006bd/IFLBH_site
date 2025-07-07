4b9bff6779628857afe063dcc4149932d401f528
Abdullah Al Amin <aaa006bd@gmail.com>
2025-07-07 21:33:02 +0600

    Image export next.config.ts
    added image export for static site

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   /* config options here */
   devIndicators: false,
   output: 'export', // Enables static export
+  images: {
+    unoptimized: true, // Required for static exports
+  },
 };
 
 export default nextConfig;
